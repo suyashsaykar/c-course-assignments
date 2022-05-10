df2cecccf3e189fc0f41de22ff13114ff22fbb78
suyash-retina1 <suyash.saykar@cctech.co.in>
2022-05-10 17:03:14 +0530

    Some changes done some are remaining

--- 030_temperature/temp-convert.c
+++ 030_temperature/temp-convert.c
@@ -2,24 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-float cToFahrenheit(float c)
+float celsiusToFahrenheit(float c)
 {
     return (c * 9 / 5) + 32;
 }
 
-float fToCelsius(float f)
+float fahrenheitToCelsius(float f)
 {
     return (f - 32) * 5 / 9;
 }
 
-float cToKelvin(float c)
+float celsiusToKelvin(float c)
 {
     return c + 273.15;
 }
 
-float kToCelsius(float k)
+float kelvinToCelsius(float k)
 {
     return k - 273.15;
+}
+
+float fahrenheitToKelvin(float f)
+{
+    return (f - 32) * 5/9 + 273.15;
+}
+
+float kelvinToFahrenheit(float k)
+{
+    return (k - 273.15) * 9/5 + 32;
 }
 
 int main(int argc, char const *argv[])
@@ -27,8 +37,8 @@ int main(int argc, char const *argv[])
     float convertTemp = 0.0;
     if (argc >= 3)
     {
-        if ((strcmp(argv[1],"C") == 0 || strcmp(argv[1],"c") == 0 || strcmp(argv[1],"Celcius") == 0 || strcmp(argv[1],"celcius") == 0 || strcmp(argv[1],"°C") == 0) &&
-           (strcmp(argv[2],"F") == 0 || strcmp(argv[2],"f") == 0 || strcmp(argv[2],"Fahrenheit") == 0 || strcmp(argv[2],"fahrenheit") == 0 || strcmp(argv[2],"°F") == 0))
+        if ((strcmp(argv[1], "C") == 0 || strcmp(argv[1], "c") == 0 || strcmp(argv[1], "Celcius") == 0 || strcmp(argv[1], "celcius") == 0 || strcmp(argv[1], "°C") == 0) &&
+           (strcmp(argv[2], "F") == 0 || strcmp(argv[2], "f") == 0 || strcmp(argv[2], "Fahrenheit") == 0 || strcmp(argv[2], "fahrenheit") == 0 || strcmp(argv[2], "°F") == 0))
         {
             if (argc == 3)
             {
@@ -36,13 +46,13 @@ int main(int argc, char const *argv[])
             }
             for (size_t tempArg = 3; tempArg < argc; tempArg++)
             {
-                convertTemp = cToFahrenheit(atof(argv[tempArg]));
+                convertTemp = celsiusToFahrenheit(atof(argv[tempArg]));
                 printf("%.2fC :%.2fF\n",atof(argv[tempArg]),convertTemp);            
             }
         }
 
-        else if ((strcmp(argv[2],"C") == 0 || strcmp(argv[2],"c") == 0 || strcmp(argv[2],"Celcius") == 0 || strcmp(argv[2],"celcius") == 0 || strcmp(argv[2],"°C") == 0) &&
-           (strcmp(argv[1],"F") == 0 || strcmp(argv[1],"f") == 0 || strcmp(argv[1],"Fahrenheit") == 0 || strcmp(argv[1],"fahrenheit") == 0 || strcmp(argv[1],"°F") == 0))
+        else if ((strcmp(argv[2], "C") == 0 || strcmp(argv[2], "c") == 0 || strcmp(argv[2], "Celcius") == 0 || strcmp(argv[2], "celcius") == 0 || strcmp(argv[2], "°C") == 0) &&
+           (strcmp(argv[1], "F") == 0 || strcmp(argv[1], "f") == 0 || strcmp(argv[1], "Fahrenheit") == 0 || strcmp(argv[1], "fahrenheit") == 0 || strcmp(argv[1], "°F") == 0))
         {
             if (argc == 3)
             {
@@ -50,13 +60,13 @@ int main(int argc, char const *argv[])
             }
             for (size_t tempArg = 3; tempArg < argc; tempArg++)
             {
-                convertTemp = fToCelsius(atof(argv[tempArg]));
+                convertTemp = fahrenheitToCelsius(atof(argv[tempArg]));
                 printf("%.2fF :%.2fC\n",atof(argv[tempArg]),convertTemp);            
             }
         }
 
-        else if ((strcmp(argv[1],"C") == 0 || strcmp(argv[1],"c") == 0 || strcmp(argv[1],"Celcius") == 0 || strcmp(argv[1],"celcius") == 0 || strcmp(argv[1],"°C") == 0) &&
-           (strcmp(argv[2],"K") == 0 || strcmp(argv[2],"k") == 0 || strcmp(argv[2],"Kelvin") == 0 || strcmp(argv[2],"kelvin") == 0))
+        else if ((strcmp(argv[1], "C") == 0 || strcmp(argv[1], "c") == 0 || strcmp(argv[1], "Celcius") == 0 || strcmp(argv[1], "celcius") == 0 || strcmp(argv[1], "°C") == 0) &&
+           (strcmp(argv[2], "K") == 0 || strcmp(argv[2], "k") == 0 || strcmp(argv[2], "Kelvin") == 0 || strcmp(argv[2], "kelvin") == 0))
         {
             if (argc == 3)
             {
@@ -64,13 +74,13 @@ int main(int argc, char const *argv[])
             }
             for (size_t tempArg = 3; tempArg < argc; tempArg++)
             {
-                convertTemp = cToKelvin(atof(argv[tempArg]));
+                convertTemp = celsiusToKelvin(atof(argv[tempArg]));
                 printf("%.2fC :%.2fK\n",atof(argv[tempArg]),convertTemp);            
             }
         }
 
-        else if ((strcmp(argv[2],"C") == 0 || strcmp(argv[2],"c") == 0 || strcmp(argv[2],"Celcius") == 0 || strcmp(argv[2],"celcius") == 0 || strcmp(argv[2],"°C") == 0) &&
-                 (strcmp(argv[1],"K") == 0 || strcmp(argv[1],"k") == 0 || strcmp(argv[1],"Kelvin") == 0 || strcmp(argv[1],"kelvin") == 0))
+        else if ((strcmp(argv[2], "C") == 0 || strcmp(argv[2], "c") == 0 || strcmp(argv[2], "Celcius") == 0 || strcmp(argv[2], "celcius") == 0 || strcmp(argv[2], "°C") == 0) &&
+                 (strcmp(argv[1], "K") == 0 || strcmp(argv[1], "k") == 0 || strcmp(argv[1], "Kelvin") == 0 || strcmp(argv[1], "kelvin") == 0))
         {
             if (argc == 3)
             {
@@ -78,11 +88,38 @@ int main(int argc, char const *argv[])
             }
             for (size_t tempArg = 3; tempArg < argc; tempArg++)
             {
-                convertTemp = kToCelsius(atof(argv[tempArg]));
+                convertTemp = kelvinToCelsius(atof(argv[tempArg]));
                 printf("%.2fK :%.2fC\n",atof(argv[tempArg]),convertTemp);            
             }
         }
+
+        else if ((strcmp(argv[1], "F") == 0 || strcmp(argv[1], "f") == 0 || strcmp(argv[1], "Fahrenheit") == 0 || strcmp(argv[1], "fahrenheit") == 0 || strcmp(argv[1], "°F") == 0) &&
+           (strcmp(argv[2], "K") == 0 || strcmp(argv[2], "k") == 0 || strcmp(argv[2], "Kelvin") == 0 || strcmp(argv[2], "kelvin") == 0))
+        {
+            if (argc == 3)
+            {
+                puts("");
+            }
+            for (size_t tempArg = 3; tempArg < argc; tempArg++)
+            {
+                convertTemp = fahrenheitToKelvin(atof(argv[tempArg]));
+                printf("%.2fF :%.2fK\n",atof(argv[tempArg]),convertTemp);            
+            }
+        }
         
+        else if ((strcmp(argv[1], "K") == 0 || strcmp(argv[1], "k") == 0 || strcmp(argv[1], "Kelvin") == 0 || strcmp(argv[1], "kelvin") == 0) &&
+           (strcmp(argv[2], "F") == 0 || strcmp(argv[2], "f") == 0 || strcmp(argv[2], "Fahrenheit") == 0 || strcmp(argv[2], "fahrenheit") == 0 || strcmp(argv[2], "°F") == 0))
+        {
+            if (argc == 3)
+            {
+                puts("");
+            }
+            for (size_t tempArg = 3; tempArg < argc; tempArg++)
+            {
+                convertTemp = kelvinToFahrenheit(atof(argv[tempArg]));
+                printf("%.2fK :%.2fF\n",atof(argv[tempArg]),convertTemp);            
+            }
+        }
         else
         {
             fprintf(stderr,"%s: error: Not enough arguments.\n Usage: %s INPUT_SCALE OUTPUT_SCALE [TEMPERATURE]...",argv[0],argv[0]);
